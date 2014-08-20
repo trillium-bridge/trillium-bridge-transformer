b8c83b1e7f30769c3cb56254ce5601e5e1473118
Kevin Peterson <kevin.peterson@mayo.edu>
2014-08-19 23:18:18 -0500

    Added a version properties file.

--- trillium-bridge-transformer-cli/src/main/java/edu/mayo/trilliumbridge/cli/AbstractBaseCliLauncher.java
+++ trillium-bridge-transformer-cli/src/main/java/edu/mayo/trilliumbridge/cli/AbstractBaseCliLauncher.java
@@ -3,6 +3,11 @@ package edu.mayo.trilliumbridge.cli;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  */
@@ -11,7 +16,10 @@ public abstract class AbstractBaseCliLauncher {
     @Option(name="-h", aliases={"-help","--help"}, usage ="Print this message")
     private boolean help = false;
 
-    public void doMain(String[] args) {
+    @Option(name="-v", aliases={"-version","--version"}, usage ="Print the application version")
+    private boolean version;
+
+    public void doMain(String[] args) throws IOException {
         System.setProperty("log4j.configuration", "trilliumbridge-cli-log4j.xml");
 
         CmdLineParser parser = new CmdLineParser(this);
@@ -20,17 +28,24 @@ public abstract class AbstractBaseCliLauncher {
         // here 80 is also the default
         parser.setUsageWidth(80);
 
-        if(this.help){
-            parser.printUsage(System.out);
-            return;
-        }
-
         try {
             // parse the arguments.
             parser.parseArgument(args);
         } catch( CmdLineException e ) {
             System.err.print(e.getMessage());
             parser.printUsage(System.err);
+            return;
+        }
+
+        if(this.help){
+            parser.printUsage(System.out);
+            return;
+        }
+
+        if(this.version){
+            Properties properties = PropertiesLoaderUtils.loadProperties(
+                    new ClassPathResource("/trilliumbridge-version.properties"));
+            System.out.println(properties.getProperty("app.version"));
             return;
         }
 
--- trillium-bridge-transformer-cli/src/main/java/edu/mayo/trilliumbridge/cli/Ccda2EpsosLauncher.java
+++ trillium-bridge-transformer-cli/src/main/java/edu/mayo/trilliumbridge/cli/Ccda2EpsosLauncher.java
@@ -1,15 +1,8 @@
 package edu.mayo.trilliumbridge.cli;
 
-import org.kohsuke.args4j.Option;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.File;
 import java.io.IOException;
 
 public class Ccda2EpsosLauncher extends AbstractBaseCliLauncher {
-
-    @Option(name="-f", required=true, usage="...")
-    private File xml;
 
     public static void main(String[] args) throws IOException {
         new Ccda2EpsosLauncher().doMain(args);
@@ -17,11 +10,6 @@ public class Ccda2EpsosLauncher extends AbstractBaseCliLauncher {
 
     @Override
     protected void run() throws Exception {
-        ClassPathXmlApplicationContext context =
-            new ClassPathXmlApplicationContext("qdm-executor-context.xml");
 
-        context.registerShutdownHook();
-
-        //
     }
 }
