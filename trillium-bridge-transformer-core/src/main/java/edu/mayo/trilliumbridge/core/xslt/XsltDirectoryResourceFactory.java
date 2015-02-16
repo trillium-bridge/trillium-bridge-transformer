b58d97575ab8e3398f2abac3efd72186e0738e73
Kevin Peterson <kevin.peterson@mayo.edu>
2015-02-15 22:37:01 -0600

    Adjusted XsltDirectoryResourceFactory to resolve URL resources.

--- trillium-bridge-transformer-core/src/main/java/edu/mayo/trilliumbridge/core/xslt/XsltDirectoryResourceFactory.java
+++ trillium-bridge-transformer-core/src/main/java/edu/mayo/trilliumbridge/core/xslt/XsltDirectoryResourceFactory.java
@@ -3,8 +3,10 @@ package edu.mayo.trilliumbridge.core.xslt;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 
 import java.io.File;
+import java.net.MalformedURLException;
 
 /**
  */
@@ -24,7 +26,13 @@ public class XsltDirectoryResourceFactory {
     }
 
     protected Resource getResource(String path) {
-        if (this.envXsltDirOverride != null) {
+        if(path.startsWith("http:")) {
+            try {
+                return new UrlResource(path);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (this.envXsltDirOverride != null) {
             return new FileSystemResource(this.envXsltDirOverride + File.separator + path);
         } else {
             return new ClassPathResource(path);
